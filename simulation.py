ea78e6edcecd07d1ba3df4c47e8cf973a2f11a58
Shahriar-Hossein <shahriar2507@gmail.com>
2025-06-01 06:33:58 +0600

    modified map and co ordinates

--- simulation.py
+++ simulation.py
@@ -21,11 +21,22 @@ currentGreen = 0   # Indicates which signal is green currently
 nextGreen = (currentGreen+1)%noOfSignals    # Indicates which signal will turn green next
 currentYellow = 0   # Indicates whether yellow signal is on or off 
 
-speeds = {'car':2.25, 'bus':1.8, 'truck':1.8, 'bike':2.5}  # average speeds of vehicles
-
+# speeds = {'car':2.25, 'bus':1.8, 'truck':1.8, 'bike':2.5}  # average speeds of vehicles
+speeds = {'car':0.65, 'bus':0.4, 'truck':0.4, 'bike':.75}  # average speeds of vehicles
 # Coordinates of vehicles' start
-x = {'right':[0,0,0], 'down':[755,727,697], 'left':[1400,1400,1400], 'up':[602,627,657]}    
-y = {'right':[348,370,398], 'down':[0,0,0], 'left':[498,466,436], 'up':[800,800,800]}
+x = {
+    'right':[0, 0, 0], 
+    'down':[710, 660, 610], 
+    'left':[1500, 1500, 1500], 
+    'up':[300, 350, 400]
+}
+y = {
+    'right':[310, 360, 410], 
+    'down':[0, 0, 0], 
+    'left':[610, 560, 510], 
+    'up':[1400, 1400, 1400]
+}
+
 
 vehicles = {'right': {0:[], 1:[], 2:[], 'crossed':0}, 'down': {0:[], 1:[], 2:[], 'crossed':0}, 'left': {0:[], 1:[], 2:[], 'crossed':0}, 'up': {0:[], 1:[], 2:[], 'crossed':0}}
 vehicleTypes = {0:'car', 1:'bus', 2:'truck', 3:'bike'}
@@ -34,21 +45,34 @@ directionNumbers = {0:'right', 1:'down', 2:'left', 3:'up'}
 # for keeping vehicle count before cycle starts
 vehicle_counts = {'right': 0, 'down': 0, 'left': 0, 'up': 0}
 
-# Define position to show vehicle count text for each signal
-vehicleCountCoods = [(500, 180), (780, 180), (780, 520), (500, 520)]
-
 # Font (reuse or redefine if needed)
 font = pygame.font.Font(None, 28)
 vehicleCountTexts = ["", "", "", ""]
 
 
-# Coordinates of signal image, timer, and vehicle count
-signalCoods = [(530,230),(810,230),(810,570),(530,570)]
-signalTimerCoods = [(530,210),(810,210),(810,550),(530,550)]
+# Adjust these to align with traffic light poles in the background
+signalCoods = [(200,210), (720,155), (790,710), (270,770)]
 
-# Coordinates of stop lines
-stopLines = {'right': 590, 'down': 330, 'left': 800, 'up': 535}
-defaultStop = {'right': 580, 'down': 320, 'left': 810, 'up': 545}
+# Adjust timer positions near signals
+signalTimerCoods = [(205,190), (725,135), (795,690), (275,750)]
+
+# Align vehicle count displays better
+vehicleCountCoods = [(110, 270), (760, 210), (830, 770), (180, 830)]
+
+# Adjust stop lines for better traffic flow alignment
+stopLines = {
+    'right': 240,  # x position where vehicles moving right should stop
+    'down': 240,   # y position for vehicles moving down
+    'left': 780,   # x position for vehicles moving left
+    'up': 750      # y position for vehicles moving up
+}
+
+defaultStop = {
+    'right': 230,  # slightly before stop line
+    'down': 230,
+    'left': 790,
+    'up': 760
+}
 # stops = {'right': [580,580,580], 'down': [320,320,320], 'left': [810,810,810], 'up': [545,545,545]}
 
 # Gap between vehicles
@@ -241,12 +265,12 @@ class Main:
     white = (255, 255, 255)
 
     # Screensize 
-    screenWidth = 1400
-    screenHeight = 800
+    screenWidth = 1008
+    screenHeight = 1000
     screenSize = (screenWidth, screenHeight)
 
     # Setting background image i.e. image of intersection
-    background = pygame.image.load('images/intersection.png')
+    background = pygame.image.load('images/city_intersection.png')
 
     screen = pygame.display.set_mode(screenSize)
     pygame.display.set_caption("SIMULATION")
